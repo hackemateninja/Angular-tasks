a726ab4d3a912ff71d06d8916d8cea5f8502b04f
hackemate_ninja <nitrowitty@gmail.com>
2020-02-16 05:19:56 -0600

    auth email

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,18 +1,19 @@
+import { ReactiveFormsModule } from '@angular/forms';
+import { EmailLoginComponent } from './email-login/email-login.component';
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
-import { UserRoutingModule } from './user-routing.module';
 import { LoginPageComponent } from './login-page/login-page.component';
-import { SharedModule } from '../shared/shared.module';
 import { GoogleSigninDirective } from './google-signin.directive';
-
+import { CommonModule } from '@angular/common';
+import { UserRoutingModule } from './user-routing.module';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [LoginPageComponent, GoogleSigninDirective],
+  declarations: [LoginPageComponent, GoogleSigninDirective, EmailLoginComponent],
   imports: [
     CommonModule,
     UserRoutingModule,
-    SharedModule
+    SharedModule,
+    ReactiveFormsModule
   ]
 })
 export class UserModule { }
