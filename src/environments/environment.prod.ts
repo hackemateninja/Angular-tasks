052afdd1300c5be52e133b6239ed2b2ebc3cc44e
hackemate_ninja <nitrowitty@gmail.com>
2020-02-16 04:41:57 -0600

    lazy load

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDptPE-seG3GibeZ0Ad-wI24z9vhqKWXGE',
+    authDomain: 'taks-68ed2.firebaseapp.com',
+    databaseURL: 'https://taks-68ed2.firebaseio.com',
+    projectId: 'taks-68ed2',
+    storageBucket: 'taks-68ed2.appspot.com',
+    messagingSenderId: '118183019618',
+    appId: '1:118183019618:web:c5f81f7ac216344fe6fd3b'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDptPE-seG3GibeZ0Ad-wI24z9vhqKWXGE',
+    authDomain: 'taks-68ed2.firebaseapp.com',
+    databaseURL: 'https://taks-68ed2.firebaseio.com',
+    projectId: 'taks-68ed2',
+    storageBucket: 'taks-68ed2.appspot.com',
+    messagingSenderId: '118183019618',
+    appId: '1:118183019618:web:c5f81f7ac216344fe6fd3b'
+  }
 };
 
 /*
